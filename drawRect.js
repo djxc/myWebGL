52054cdf3bb9482d8026083fed96fe286ac39cd4
dj_jxc <dj_jxc@163.com>
2020-11-23 09:50:24 +0800

    test webgl

--- drawRect.js
+++ drawRect.js
@@ -1,29 +1,66 @@
+/**
+ * @FileDescription 学习webgl
+ * webgl利用canvas作为基础绘制三维图型，没有webgl，canvas只能绘制二维图形；
+ * webgl利用canvas可以绘制三维图形
+ * @Author small dj
+ * @Date 2020-11-21
+ * @LastEditor small dj
+ * @LastEditTime 2020-11-21 11:12
+ */
 
-var ctx
 
+var ctx;		// 定义画布上下文的全局变量
+
+/**
+ * **在html中利用h5新特性canvas绘制图像**  
+ * 1、canvas可以动态绘制图像
+ * 2、具体步骤为：1) 获取canvas元素;2) 获取canvas的上下文; 3) 在上下文中绘制图像
+ */
 function main() {
+	// 获取canvas元素，检查其是否存在
 	var canvas = document.getElementById('example')
 	if (!canvas) {
 		console.log('Failed to retrieve the <canvas> element')
 		return
 	}
-	//ctx = canvas.getContext('2d')
+	testWebGL(canvas)
+	
+	
+}
+
+/**
+ * **测试canvas**
+ */
+function testCanvas(canvas) {
+	ctx = canvas.getContext('2d')
+	if (!ctx) {
+		console.log('Failed to get the rendering context for WebGL')
+		return
+	}
+	drawR(ctx)
+}
+
+/**
+ * **测试webgl**
+ */
+function testWebGL(canvas) {
 	ctx = getWebGLContext(canvas)
 	if (!ctx) {
 		console.log('Failed to get the rendering context for WebGL')
 		return
 	}
+	clearColor()
 }
 
-// 绘制一个矩形
-function drawR() {
-	ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'
+// 利用canvas绘制一个矩形
+function drawR(ctx) {
+	ctx.fillStyle = 'rgba(100, 100, 255, 0.3)'
 	ctx.fillRect(120, 10, 150, 150)
 }
 
-// 清空<canvas>的颜色
+// webgl清空<canvas>的颜色
 function clearColor(){
-	ctx.clearColor(0.0, 0.0, 0.0, 1.0)	
+	ctx.clearColor(0.0, 1.0, 0.0, 0.2)	
 	ctx.clear(ctx.COLOR_BUFFER_BIT)
 }
 
