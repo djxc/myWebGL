16a679f3da2e92b6688457c1afe8db19ef236d23
djxc <dj_jxc@163.com>
2020-12-15 17:44:49 +0800

    2020-12-15

--- src/drawPoints.js
+++ src/drawPoints.js
@@ -16,7 +16,7 @@ import { initShaders } from './lib/cuon-utils';
  * @return string[]
  */
 function createdShader() {
-    // 定点着色器
+    // 定点着色器,包括顶点的位置以及顶点的大小
     var VSHADER_SOURCE =
         'attribute vec4 a_Position;\n' +
         'attribute float a_PointSize;\n' +
@@ -24,7 +24,7 @@ function createdShader() {
         ' gl_Position = a_Position;\n' +
         ' gl_PointSize = a_PointSize;\n' +
         '}\n'
-    // 片元着色器
+    // 片元着色器，颜色
     var FSHADER_SOURCE =
         'precision mediump float;\n' +
         'uniform vec4 u_FragColor;\n' +
@@ -39,22 +39,9 @@ function createdShader() {
  * @param {float[]} point_position 点坐标
  * @param {float} point_size 点大小，浮点型
  */
-function draw_point(ctx, point_position, point_size) {
-    // 定点着色器
-    var VSHADER_SOURCE =
-        'attribute vec4 a_Position;\n' +
-        'attribute float a_PointSize;\n' +
-        'void main(){\n' +
-        ' gl_Position = a_Position;\n' +
-        ' gl_PointSize = a_PointSize;\n' +
-        '}\n'
-    // 片元着色器
-    var FSHADER_SOURCE =
-        'precision mediump float;\n' +
-        'uniform vec4 u_FragColor;\n' +
-        'void main() {\n' +
-        ' gl_FragColor = u_FragColor;\n' +
-        '}\n'
+function draw_point(ctx, point_position, point_size) {  
+    // 获取着色器
+    let [VSHADER_SOURCE, FSHADER_SOURCE] = createdShader()
     if (!initShaders(ctx, VSHADER_SOURCE, FSHADER_SOURCE)) {
         console.log('Failed to initialize shaders')
         return
@@ -97,6 +84,10 @@ export function manualDrawPoints(canvas, ctx) {
 /**
  * **在缓冲区内写入多个点**  
  * 1、创建缓冲区
+ * 2、绑定缓冲区
+ * 3、向缓冲区填写数据
+ * 4、获取着色器中的变量
+ * 5、给着色器变量赋值
  * @param {*} gl 
  * @return n 点的个数
  */
@@ -106,16 +97,25 @@ function initVertexBuffers(gl) {
     ])
     console.log(vertices.length / 2);
     let n = vertices.length / 2;
-    let vertexBuffer = gl.createBuffer();       // 创建缓冲区
+    let vertexBuffer = gl.createBuffer();                                   // 创建缓冲区
     if (!vertexBuffer) {
         console.log('Failed to create the buffer object');
         return -1;
     }
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);       // 绑定缓冲区到ARRAY_BUFFER
-    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);       // 向缓冲区写入数据(即为将数据写入ARRAY_BUFFER)
-    // 获取着色器中的变量，然后将缓冲区分配给着色器变量
-    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);   // 2表示每个点个数据个数，这里为xy坐标，每个点只有两个数据
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);                           // 绑定缓冲区到ARRAY_BUFFER
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);               // 向缓冲区写入数据(即为将数据写入ARRAY_BUFFER)
+    
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');        // 获取着色器中的变量，然后将缓冲区分配给着色器变量
+    var a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');      // 获取着色器中的顶点大小变量
+    // 如果获取着色器中的变量失败，给出提示
+    if (a_PointSize < 0 || a_Position < 0) {
+        console.log("Failed to get the storage location of a_Position or a_PointSize");
+        return
+    }
+
+    gl.vertexAttrib1f(a_PointSize, 8.0);                                    // 给缓冲区的点大小变量赋值
+    // 给缓冲区内的点数组赋值，2表示每个点个数据个数，这里为xy坐标，每个点只有两个数据
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);           
     gl.enableVertexAttribArray(a_Position);
 
     return n;
@@ -145,15 +145,9 @@ export function drawMultiPoints(ctx) {
         return
     }
     // 获取着色器中的变量，为方便对其赋值
-    var a_PointSize = ctx.getAttribLocation(ctx.program, 'a_PointSize');
     var u_FragColor = ctx.getUniformLocation(ctx.program, 'u_FragColor');
+    ctx.uniform4f(u_FragColor, 0.6, 0.5, 0.6, 0.78);
 
-    if (a_PointSize < 0) {
-        console.log("Failed to get the storage location of a_Position");
-        return
-    }
-    ctx.vertexAttrib1f(a_PointSize, 8.0);
-    ctx.uniform4f(u_FragColor, 0.3, 0.5, 0.6, 0.78);
     clearColor(ctx)
     // 第一个参数为绘制的类型，这里为点；第二个参数为从第几个点开始绘制；第二个参数为为绘制点的个数
     // ctx.drawArrays(ctx.POINTS, 1, 1)
